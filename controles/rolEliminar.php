15e6413ec580ed77549b9a46bec4328f466c6709
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2022-10-05 15:28:36 -0300

    script sql

--- controles/rolEliminar.php
+++ controles/rolEliminar.php
@@ -2,13 +2,14 @@
     require_once ('../class/Roles.php');
     if(  isset($_GET['id']) ){
         $id = $_GET['id'];
+        echo $id;
 
         $rol = new Rol();
         $rol->setId($id);
 
         $rol->borrar();
 
-        header('location: ../roles/');
+        header('location: ../roles/index.php');
     }
 
 ?>
--- docs/blog.sql
+++ docs/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-10-2022 a las 20:25:37
+-- Tiempo de generación: 05-10-2022 a las 20:28:16
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.1
 
@@ -41,6 +41,29 @@ INSERT INTO `roles` (`id_rol`, `descripcion`) VALUES
 (4, 'prueba'),
 (6, 'otro rol');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(64) NOT NULL,
+  `apellido` varchar(64) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `email`, `password`) VALUES
+(1, 'juan', 'Ruiz', 'juan@gmail.com', '123213'),
+(2, 'juan', 'Ruiz', 'juan@gmail.com', '123213'),
+(3, 'Mateo', 'Herrera', 'mateo@gmail.com', '123213');
+
 --
 -- Índices para tablas volcadas
 --
@@ -52,6 +75,12 @@ ALTER TABLE `roles`
   ADD PRIMARY KEY (`id_rol`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -60,6 +89,12 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
